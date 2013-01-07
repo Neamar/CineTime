6926f72d86082c11b9933668848d19ccd8e6eded
Neamar <neamar@neamar.fr>
2013-01-07 22:01:13 +0100

    Test input / prompt

--- src/fr/neamar/cinetime/TheatersActivity.java
+++ src/fr/neamar/cinetime/TheatersActivity.java
@@ -1,20 +1,50 @@
 package fr.neamar.cinetime;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class TheatersActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_theaters);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_theaters);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_theaters, menu);
-        return true;
-    }
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+		alert.setTitle(getString(R.string.ui_add_theater_title));
+		alert.setMessage(getString(R.string.ui_add_theater_desc));
+
+		// Set an EditText view to get user input
+		final EditText input = new EditText(this);
+		input.setInputType(InputType.TYPE_CLASS_NUMBER);
+		alert.setView(input);
+
+		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int whichButton) {
+				String value = input.getText().toString();
+				// Do something with value!
+			}
+		});
+
+		alert.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						// Canceled.
+					}
+				});
+
+		alert.show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_theaters, menu);
+		return true;
+	}
 }
