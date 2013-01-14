bc54729d6b3d1bb26d636f4dc6b2eb52875087e6
Neamar <neamar@neamar.fr>
2013-01-14 23:38:06 +0100

    Add activity for details view

---
+++ src/fr/neamar/cinetime/DetailsActivity.java
@@ -0,0 +1,12 @@
+package fr.neamar.cinetime;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailsActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_details);
+	}
+}
