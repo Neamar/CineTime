6f89b30a8d5c0705d9c86530b9ea789ebec0a86f
Neamar <neamar@neamar.fr>
2013-11-25 15:32:28 +0100

    Cleanup

--- src/fr/neamar/cinetime/TheatersActivity.java
+++ src/fr/neamar/cinetime/TheatersActivity.java
@@ -1,30 +1,18 @@
 package fr.neamar.cinetime;
 
-import android.content.Intent;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.widget.Toast;
 
 import com.google.analytics.tracking.android.EasyTracker;
 
-import fr.neamar.cinetime.fragments.TheatersFragment;
-
-public class TheatersActivity extends FragmentActivity implements TheatersFragment.Callbacks {
-
-	TheatersFragment theatersFragment;
-	static String title = "";
-
+public class TheatersActivity extends ListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_theaters_list);
-		if (title.equalsIgnoreCase("")) {
-			title = getResources().getString(R.string.title_activity_theaters);
-		}
-		setTitle(title);
+		setContentView(R.layout.activity_theaters);
+		setTitle(R.string.title_activity_theaters);
 	}
 	
 
@@ -33,36 +21,6 @@ public class TheatersActivity extends FragmentActivity implements TheatersFragme
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.activit_theaters, menu);
 		return true;
-	}
-
-	@Override
-	public void onBackPressed() {
-		if (theatersFragment.goBack()) {
-			super.onBackPressed();
-		}
-	}
-
-	@Override
-	public void onItemSelected(int position, Fragment source) {
-		String code = theatersFragment.getTheaters().get(position).code;
-
-		String title = theatersFragment.getTheaters().get(position).title;
-
-		Intent intent = new Intent(this, MoviesActivity.class);
-		intent.putExtra("code", code);
-		intent.putExtra("theater", title);
-		startActivity(intent);
-	}
-
-	@Override
-	public void setFragment(Fragment fragment) {
-		theatersFragment = (TheatersFragment) fragment;
-	}
-
-	@Override
-	public void finishNoNetwork() {
-		Toast.makeText(this, "Impossible de télécharger les données. Merci de vérifier votre connexion ou de réessayer dans quelques minutes.", Toast.LENGTH_SHORT).show();
-		finish();
 	}
 
 	@Override
@@ -75,11 +33,5 @@ public class TheatersActivity extends FragmentActivity implements TheatersFragme
 	protected void onStop() {
 		super.onStop();
 		EasyTracker.getInstance().activityStop(this);
-	}
-
-	@Override
-	public void updateTitle(String title) {
-		TheatersActivity.title = title;
-		setTitle(title);
 	}
 }
