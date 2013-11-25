eb69d7326763e7e8cb60e29359f5ff9b178ddeb8
Neamar <neamar@neamar.fr>
2013-11-25 16:09:23 +0100

    Restored search behavior

--- src/fr/neamar/cinetime/TheatersActivity.java
+++ src/fr/neamar/cinetime/TheatersActivity.java
@@ -1,26 +1,49 @@
 package fr.neamar.cinetime;
 
+import java.util.ArrayList;
+
 import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.analytics.tracking.android.EasyTracker;
 
+import fr.neamar.cinetime.objects.Theater;
+
 public class TheatersActivity extends ListActivity {
+	private ProgressDialog dialog;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_theaters);
 		setTitle(R.string.title_activity_theaters);
 	}
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.activit_theaters, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_search:
+			onSearchRequested();
+			return true;
+		case R.id.menu_search_geo:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 	@Override
@@ -33,5 +56,40 @@ public class TheatersActivity extends ListActivity {
 	protected void onStop() {
 		super.onStop();
 		EasyTracker.getInstance().activityStop(this);
+	}
+	
+	protected ArrayList<Theater> retrieveResults(String... queries) {
+		//TODO: abstract
+		return null;
+	}
+	
+	protected class LoadTheatersTask extends AsyncTask<String, Void, ArrayList<Theater>> {
+		public LoadTheatersTask() {
+			super();
+		}
+
+		@Override
+		protected void onPreExecute() {
+			if (dialog != null && dialog.isShowing()) {
+				dialog.dismiss();
+			}
+			dialog = new ProgressDialog(TheatersActivity.this);
+			dialog.setMessage("Recherche en cours...");
+			dialog.show();
+		}
+
+		@Override
+		protected ArrayList<Theater> doInBackground(String... queries) {
+			return retrieveResults(queries);
+		}
+
+		@Override
+		protected void onPostExecute(ArrayList<Theater> theaters) {
+			if (dialog != null && dialog.isShowing()) {
+				dialog.dismiss();
+			}
+			
+			setListAdapter(new TheaterAdapter(TheatersActivity.this, R.layout.listitem_theater, theaters));
+		}
 	}
 }
