1077cbb53028b71d26c43e04c887101b1afa5e8e
Neamar <neamar@neamar.fr>
2018-01-04 22:36:01 +0100

    I think I'm done

--- app/src/main/java/fr/neamar/cinetime/objects/Movie.java
+++ app/src/main/java/fr/neamar/cinetime/objects/Movie.java
@@ -100,13 +100,13 @@ public class Movie implements Comparable<Movie> {
         String url = null;
         switch (level) {
             case 1:
-                url = "http://images.allocine.fr/r_150_500" + poster;
+                url = "http://images.allocine.fr/r_215_290" + poster;
                 break;
             case 2:
                 url = "http://images.allocine.fr/r_300_999" + poster;
                 break;
             case 3:
-                url = "http://images.allocine.fr/r_720_2400" + poster;
+                url = "http://images.allocine.fr/r_1280_2000" + poster;
                 break;
             default:
                 url = "";
