823323fd4c8109e75a06d31003258eb31a50d551
Nicolas B <34476051+Nico04@users.noreply.github.com>
2020-11-01 11:09:50 +0100

    update version & package name

--- android/app/src/main/kotlin/fr/neamar/cinetime/MainActivity.kt
+++ android/app/src/main/kotlin/fr/neamar/cinetime/MainActivity.kt
@@ -1,10 +1,11 @@
-package fr.neamar.cinetime.flutter
+package fr.neamar.cinetime
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+// TODO migrate. See https://github.com/flutter/flutter/wiki/Upgrading-pre-1.12-Android-projects
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
