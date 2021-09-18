e1320d6f62a921c0746f79898b81ba2b2d37bfce
Nico04 <34476051+Nico04@users.noreply.github.com>
2021-09-18 09:35:50 +0200

    comments

--- lib/widgets/show_more.dart
+++ lib/widgets/show_more.dart
@@ -31,7 +31,7 @@ class _ShowMoreTextState extends State<ShowMoreText> {
           if (isExpanded) {
             return text;
           } else {
-            return ShaderMask(
+            return ShaderMask(    // Draw a gradient overlay would probably be more performant, but ShaderMask works on all backgrounds.
               shaderCallback: (rect) {
                 return const LinearGradient(
                   begin: Alignment.center,
@@ -64,103 +64,3 @@ class _ShowMoreTextState extends State<ShowMoreText> {
     );
   }
 }
-
-/** TODO remove ?
-/// Copied from flutter_show_more 0.1.2
-/// - set onTap behaviour on all text
-/// - add AnimatedSize
-class ShowMoreText extends StatefulWidget {
-  final String text;
-  final int maxLength;
-  final String showMoreText;
-  final TextStyle style;
-  final TextStyle showMoreStyle;
-  final bool shouldShowLessText;
-  final String showLessText;
-
-  const ShowMoreText(
-      this.text, {
-        Key key,
-        this.maxLength: 100,
-        this.showMoreText,
-        this.style,
-        this.showMoreStyle,
-        this.shouldShowLessText: false,
-        this.showLessText,
-      })  : assert(text != null),
-        assert(maxLength != null),
-        assert(shouldShowLessText != null),
-        super(key: key);
-
-  @override
-  _ShowMoreTextState createState() => _ShowMoreTextState();
-}
-
-class _ShowMoreTextState extends State<ShowMoreText> with SingleTickerProviderStateMixin {
-  bool isExpanded = false;
-
-  @override
-  Widget build(BuildContext context) {
-    if (widget.text.length <= widget.maxLength) {
-      return Text(widget.text, style: widget.style);
-    }
-
-    var showMoreStyle = widget.showMoreStyle ??
-        Theme.of(context).textTheme.body2.copyWith(
-          color: Theme.of(context).accentColor,
-        );
-
-    Widget child;
-
-    if (isExpanded) {
-      if (widget.shouldShowLessText) {
-        child = Text.rich(
-          TextSpan(
-            style: widget.style,
-            children: [
-              TextSpan(text: widget.text),
-              TextSpan(text: ' '),
-              TextSpan(
-                text: widget.showMoreText ?? 'less',
-                style: showMoreStyle,
-              ),
-            ],
-          ),
-        );
-      } else {
-        child = Text(widget.text, style: widget.style);
-      }
-    } else {
-      var substring = widget.text.substring(0, widget.maxLength);
-
-      child = Text.rich(
-        TextSpan(
-          style: widget.style,
-          children: [
-            TextSpan(text: substring),
-            TextSpan(text: '... '),
-            TextSpan(
-              text: widget.showMoreText ?? 'more',
-              style: showMoreStyle,
-            ),
-          ],
-        ),
-      );
-    }
-
-    return GestureDetector(
-      child: AnimatedSize(
-        duration: Duration(milliseconds: 250),
-        alignment: Alignment.topLeft,
-        child: child,
-        vsync: this,
-      ),
-      onTap: () {
-        setState(() {
-          isExpanded = !isExpanded;
-        });
-      },
-    );
-  }
-}
-*/
