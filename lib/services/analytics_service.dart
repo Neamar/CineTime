cdaf722011343a24299339b63ae3bff9dc085773
Nico04 <34476051+Nico04@users.noreply.github.com>
2021-10-27 17:21:58 +0200

    disable amplitude for android api <21

--- lib/services/analytics_service.dart
+++ lib/services/analytics_service.dart
@@ -1,18 +1,25 @@
 import 'package:amplitude_flutter/amplitude.dart';
+import 'package:cinetime/main.dart';
 import 'package:flutter/foundation.dart';
 
 class AnalyticsService {
   static const _amplitudeKey = 'af30e5f865dd7eb9442d6a491cbac02f';
   static const _amplitudeDevKey = '845fcba9d80e7ead8619bba849d7243e';
 
-  static final _amplitude = Amplitude.getInstance();
+  static Amplitude? _amplitude;
 
   static Future<void> init() async {
-    await _amplitude.init(kReleaseMode ? _amplitudeKey : _amplitudeDevKey);
+    // amplitude_flutter plugin requires Android API 21+.
+    // See https://github.com/amplitude/Amplitude-Flutter/issues/24
+    if (App.androidSdkVersion < 21) return;
+
+    _amplitude = Amplitude.getInstance();
+    await _amplitude!.init(kReleaseMode ? _amplitudeKey : _amplitudeDevKey);
     //await _amplitude.setServerUrl('https://api.eu.amplitude.com');    // Setting EU server throw a silence "wrong apiKey" error
-    await _amplitude.enableCoppaControl();
-    await _amplitude.trackingSessionEvents(true);
+    await _amplitude!.enableCoppaControl();
+    await _amplitude!.trackingSessionEvents(true);
   }
 
-  static Future<void> trackEvent(String eventName, [Map<String, dynamic>? properties]) => _amplitude.logEvent(eventName, eventProperties: properties);
+  static Future<void> trackEvent(String eventName, [Map<String, dynamic>? properties]) async =>
+      await _amplitude?.logEvent(eventName, eventProperties: properties);
 }
