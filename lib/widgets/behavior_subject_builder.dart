eed68d5fed8dd54261667e8f89758510a8e6ad93
Nico04 <34476051+Nico04@users.noreply.github.com>
2022-01-04 11:42:55 +0100

    revert rxdart to 0.26.0 update packages

--- lib/widgets/behavior_subject_builder.dart
+++ lib/widgets/behavior_subject_builder.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:rxdart/subjects.dart';
+import 'package:rxdart/rxdart.dart';
 
 class BehaviorSubjectBuilder<T> extends StreamBuilder<T> {
   BehaviorSubjectBuilder({ Key? key, required BehaviorSubject<T> subject, required AsyncWidgetBuilder<T> builder }) : super(
     key: key,
     stream: subject,
-    initialData: subject.valueOrNull,
+    initialData: subject.value,
     builder: builder,
   );
 }
