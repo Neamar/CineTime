2b6120943003b1fa9e88f86cb4b2b54638d65b80
Nico04 <34476051+Nico04@users.noreply.github.com>
2025-01-13 10:56:35 +0100

    Handle small texts in ShowMoreText

--- lib/widgets/show_more.dart
+++ lib/widgets/show_more.dart
@@ -20,50 +20,73 @@ class _ShowMoreTextState extends State<ShowMoreText> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: CtAnimatedSwitcher(
-        sizeAnimation: true,
-        child: () {
-          final text = RichText(
-            text: TextSpan(
-              style: context.textTheme.bodyMedium,
-              children: [
-                if (widget.header != null)...[
-                  TextSpan(text: '${widget.header!}\n', style: const TextStyle(color: AppResources.colorDarkRed, fontWeight: FontWeight.w500)),
-                  WidgetSpan(child: SizedBox(height: AppResources.spacerTiny.height, width: double.infinity)),
-                ],
-                TextSpan(text: widget.text),
-              ],
-            ),
-            textAlign: TextAlign.justify,
-            maxLines: 100,
-            overflow: TextOverflow.ellipsis,
-          );
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        // Build the text
+        final textSpan = TextSpan(
+          style: context.textTheme.bodyMedium,
+          children: [
+            if (widget.header != null)...[
+              TextSpan(text: '${widget.header!}\n', style: const TextStyle(color: AppResources.colorDarkRed, fontWeight: FontWeight.w500)),
+              const TextSpan(text: '\n', style: TextStyle(fontSize: 5)),
+            ],
+            TextSpan(text: widget.text),
+          ],
+        );
 
-          if (isExpanded) {
-            return text;
-          } else {
-            return ShaderMask(    // Draw a gradient overlay would probably be more performant, but ShaderMask works on all backgrounds.
-              shaderCallback: (rect) {
-                return const LinearGradient(
-                  begin: Alignment.center,
-                  end: Alignment.bottomCenter,
-                  colors: [Colors.black, Colors.transparent],
-                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
-              },
-              blendMode: BlendMode.dstIn,
-              child: SizedBox(
-                height: widget.collapsedHeight,
-                child: text,
-              ),
-            );
-          }
-        } (),
-      ),
-      onTap: () {
-        setState(() {
-          isExpanded = !isExpanded;
-        });
+        // Layout the text to calculate its height
+        final textPainter = TextPainter(
+          text: textSpan,
+          textDirection: TextDirection.ltr,
+          textAlign: TextAlign.justify,
+          maxLines: 100,
+        );
+        textPainter.layout(maxWidth: constraints.maxWidth);
+        final height = textPainter.size.height;
+
+        // Build the text widget
+        final text = RichText(
+          text: textSpan,
+          textAlign: TextAlign.justify,
+          maxLines: 100,
+          overflow: TextOverflow.ellipsis,
+        );
+
+        // If the text is smaller than the collapsed height, don't show the "Show more" button
+        if (height <= widget.collapsedHeight) {
+          return text;
+        }
+
+        return GestureDetector(
+          child: CtAnimatedSwitcher(
+            sizeAnimation: true,
+            child: () {
+              if (isExpanded) {
+                return text;
+              } else {
+                return ShaderMask(    // Draw a gradient overlay would probably be more performant, but ShaderMask works on all backgrounds.
+                  shaderCallback: (rect) {
+                    return const LinearGradient(
+                      begin: Alignment.center,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.black, Colors.transparent],
+                    ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                  },
+                  blendMode: BlendMode.dstIn,
+                  child: SizedBox(
+                    height: widget.collapsedHeight,
+                    child: text,
+                  ),
+                );
+              }
+            } (),
+          ),
+          onTap: () {
+            setState(() {
+              isExpanded = !isExpanded;
+            });
+          },
+        );
       },
     );
   }
