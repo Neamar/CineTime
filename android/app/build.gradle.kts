c45c5b8e87e59f071983ee5ab9bd44887f88c368
Nico04 <34476051+Nico04@users.noreply.github.com>
2025-08-19 13:29:43 +0200

    remove release key code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,3 @@
-import java.util.Properties
-import java.io.FileInputStream
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -8,11 +5,6 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
-if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-}
 android {
     namespace = "fr.neamar.cinetime"
     compileSdk = flutter.compileSdkVersion
@@ -36,18 +28,8 @@ android {
         multiDexEnabled = true
     }
 
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String?
-            keyPassword = keystoreProperties["keyPassword"] as String?
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String?
-        }
-    }
-
     buildTypes {
         release {
-            // signingConfig = signingConfigs.getByName("release")
             signingConfig = signingConfigs.getByName("debug")  // We first sign build with any debug key, real prod signing is done afterwards
 
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
